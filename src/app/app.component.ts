a3dce39ac022075572a5379839e3198bfd317c27
Lyle Dippo <lyledippo@Lyles-MBP.fios-router.home>
2017-07-20 07:58:37 -0400

    add text input to UI for dynamic gen of concordance.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import * as util from './app.util';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/startWith';
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,33 +12,46 @@ import * as util from './app.util';
 })
 export class AppComponent {
   title = 'app';
-
-  // START CONCORDANCE LOGIC
-  input = `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam nulla alias, excepturi, numquam sequi labore, modi accusamus, cum dolor nisi necessitatibus facilis ea veniam? Architecto natus adipisci laborum ex magnam cupiditate voluptates sequi ipsam commodi nemo ullam obcaecati odio assumenda soluta, omnis velit earum minima sit necessitatibus repudiandae. Sit consequatur ea quia, quos, fugiat, cum animi voluptate sequi doloribus praesentium perferendis tempora! Doloribus qui vitae voluptas. Fugiat cupiditate fuga alias fugit ipsa impedit maiores dolor quia, quae animi, maxime cumque quasi amet neque porro itaque sapiente, expedita est possimus, eligendi nesciunt. Ea excepturi, reprehenderit. Odio dolores officiis, quidem. Quaerat, nulla?`;
+  input = `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam nulla alias, excepturi, numquam sequi labore, modi accusamus, cum dolor nisi necessitatibus facilis ea veniam? Architecto natus adipisci laborum ex magnam cupiditate voluptates sequi ipsam commodi nemo ullam obcaecati 
+  odio assumenda soluta, omnis velit earum minima sit necessitatibus repudiandae. Sit consequatur ea quia, quos, fugiat, cum animi voluptate sequi doloribus praesentium perferendis tempora! Doloribus qui vitae voluptas. Fugiat cupiditate fuga alias fugit ipsa impedit maiores dolor quia, quae animi, 
+  maxime cumque quasi amet neque porro itaque sapiente, expedita est possimus, eligendi nesciunt. Ea excepturi, reprehenderit. Odio dolores officiis, quidem. Quaerat, nulla?`;
+  inputText: FormControl = new FormControl(this.input);
   collection;
 
   constructor() {
-    const JSONinput = JSON.stringify(this.input);
+    // here we watch the input in the UI so we can create a concordance real time
+    this.inputText.valueChanges
+      .debounceTime(200)
+      .startWith(this.input)
+      .subscribe(
+        val => {
+          this.input = val;
+          // START CONCORDANCE LOGIC
+          const JSONinput = JSON.stringify(this.input);
 
-    const bodySplitByLine = util.splitBodyByLine(JSONinput);
+          const bodySplitByLine = util.splitBodyByLine(JSONinput);
 
-    const wordsByLine = bodySplitByLine.map((line, idx) =>
-      util.getWords(line)
-          .map(word => ({ word: word, line: idx })));
+          const wordsByLine = bodySplitByLine.map((line, idx) =>
+            util.getWords(line)
+              .map(word => ({ word: word, line: idx })));
 
-    const flatWordsWithLinesArr = util.flattenArr(wordsByLine);
+          const flatWordsWithLinesArr = util.flattenArr(wordsByLine);
 
-    const resultObj = util.createConcordance(flatWordsWithLinesArr);
+          const resultObj = util.createConcordance(flatWordsWithLinesArr);
 
-    const resultArr = Object.keys(resultObj).map(k => resultObj[k]);
+          const resultArr = Object.keys(resultObj).map(k => resultObj[k]);
 
-    this.collection = resultArr.sort((a, b) => {
-      const wordA = String(a.word).toLowerCase();
-      const wordB = String(b.word).toLowerCase();
-      return (wordA < wordB) ? -1 : (wordA > wordB) ? 1 : 0;
-    });
+          this.collection = resultArr.sort((a, b) => {
+            const wordA = String(a.word).toLowerCase();
+            const wordB = String(b.word).toLowerCase();
+            return (wordA < wordB) ? -1 : (wordA > wordB) ? 1 : 0;
+          });
+          // END CONCORDANCE LOGIC
+        }
+    );
+
 
   }
-  // END CONCORDANCE LOGIC
+  
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { MaterialModule } from '@angular/material';
 import { AppComponent } from './app.component';
-
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     MaterialModule
   ],
   providers: [],
